51c9312cb9caeae1aea82506b71d101894eeeee7
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-03-08 12:51:53 +0100

    add

--- starter/script.js
+++
@@ -1,5 +0,0 @@
-const isConnected = "Je suis connecté";
-console.log(isConnected);
-
-let firstName = "John";
-console.log(firstName);
---
+++ starter/scripts.js
@@ -0,0 +1,16 @@
+const isConnected = "Je suis connecté";
+console.log(isConnected);
+
+// let firstName = "John";
+// console.log(firstName);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+//birthYear = 1992;
+//const job;
+
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah);
